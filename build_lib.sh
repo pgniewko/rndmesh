d1f248dc40ec23b943a487ec481d37256ac1376f
Pawel Gniewek <gniewko.pablo@gmail.com>
2016-10-30 22:49:54 -0700

    Renaming files

--- build_lib.sh
+++ build_lib.sh
@@ -1,8 +1,11 @@
 #! /bin/bash -x
 
-rm test
-gfortran -c stripack.f90
+rm main.o
 g++ -c main.cpp
+
+cd ./src/
+rm *.o
+gfortran -c stripack.f90
 g++ -c random.cpp
 g++ -c mc.c
 g++ -o test main.o stripack.o mc.o random.o -std=gnu++11 -lgfortran
